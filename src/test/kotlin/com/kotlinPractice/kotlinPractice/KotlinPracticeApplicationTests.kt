13ad127b96c7494dd1271204dca1f22fda7d4b07
Ja-e <leeniesta@naver.com>
2023-01-03 22:29:36 +0900

    Initial commit

---
+++ src/test/kotlin/com/kotlinPractice/kotlinPractice/KotlinPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinPractice.kotlinPractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
