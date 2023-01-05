95cecf50052eff95a5bb27bd67363b4f920e7305
Ja-e <leeniesta@naver.com>
2023-01-06 00:03:53 +0900

    RequestParam 실습

--- src/main/kotlin/com/kotlinPractice/kotlinPractice/controller/HelloController.kt
+++ src/main/kotlin/com/kotlinPractice/kotlinPractice/controller/HelloController.kt
@@ -3,6 +3,7 @@ package com.kotlinPractice.kotlinPractice.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 class HelloController {
@@ -11,5 +12,11 @@ class HelloController {
     fun hello(model:Model):String {
         model.addAttribute("data","hello!!")
         return "hello"
+    }
+
+    @GetMapping("hello-mvc")
+    fun helloMvc(@RequestParam(name = "name")name:String, model:Model):String {
+        model.addAttribute("name",name)
+        return "hello-mvc"
     }
 }
